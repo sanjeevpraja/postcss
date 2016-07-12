4798a8f3eb626f5dc41e5a8b6240264e901220b2
Sanjeev Prajapati <spr@codebee.dk>
2016-07-12 12:50:46 +0545

    initial vedio

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+
+var autoprefixer = require('autoprefixer');
+var cssnext = require('cssnext');
+var precss = require('precss');
+
+
+gulp.task('css', function () {
+	var processors = [
+	autoprefixer,
+	cssnext,
+	precss
+	];
+
+	return gulp.src('./src/*.css')
+	.pipe(postcss(processors))
+	.pipe(gulp.dest('./dest'));
+});
+
+
+gulp.task('watch', function(){
+	gulp.watch('./src/*.css', ['css']);
+})
