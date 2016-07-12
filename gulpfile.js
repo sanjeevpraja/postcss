944269f83996e423c9aa01d15f89d47933bc5c4a
Sanjeev Prajapati <spr@codebee.dk>
2016-07-12 15:48:44 +0545

    ..

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,27 @@
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
-
+var sass = require('gulp-sass');
 var autoprefixer = require('autoprefixer');
 var cssnext = require('cssnext');
 var precss = require('precss');
+var cssnext = require('cssnext');
 
 
 gulp.task('css', function () {
 	var processors = [
 	autoprefixer,
 	cssnext,
-	precss
+	precss,
+	cssnext({})
 	];
 
-	return gulp.src('./src/*.css')
+	return gulp.src('./src/*.scss')
+	.pipe(sass())
 	.pipe(postcss(processors))
 	.pipe(gulp.dest('./dest'));
 });
 
 
 gulp.task('watch', function(){
-	gulp.watch('./src/*.css', ['css']);
+	gulp.watch('./src/*.scss', ['css']);
 })
